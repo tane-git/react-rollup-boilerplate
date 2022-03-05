8d8bfd9f096f38f434fba892dfd2f7354d4027b6
tane-git <tane.alexander.wilson@gmail.com>
2022-03-05 13:37:28 +1300

    add antd and add it to peer dependencies

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import cx from 'classnames';
 import './styles.css';
 
+import { Button as AntdButton } from 'antd';
+
 export interface IButtonProps {
   onClick: () => void;
   disabled?: boolean;
@@ -10,7 +12,9 @@ export interface IButtonProps {
 }
 
 export const Button: FC<IButtonProps> = ({ onClick, disabled, className, text }) => (
-  <button type="button" className={cx('button', className)} onClick={onClick} disabled={disabled}>
+  // <button type="button" className={cx('button', className)} onClick={onClick} disabled={disabled}>
+  <AntdButton className={cx('button', className)} onClick={onClick} disabled={disabled}>
     {text}
-  </button>
+  </AntdButton>
+  // </button>
 );
